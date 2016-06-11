57ad8b5e9b93c7c02a74d4b6239fa959cc7e288c
Thangavel L Nathan <yel.thangavel@gmail.com>
2016-06-12 00:07:42 +0530

    Added Read Operations.

--- src/main/java/com/mongodb/crud/samples/SampleCRUDOps.java
+++ src/main/java/com/mongodb/crud/samples/SampleCRUDOps.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.util.JSON;
@@ -13,15 +14,16 @@ import com.mongodb.util.JSON;
 public class SampleCRUDOps {
     
 	public static void main(String[] args) throws UnknownHostException {
-		InsertData();
-	}
-	
-	private static void InsertData() throws UnknownHostException{
-		
 		MongoClient client = new MongoClient();
 		DB db = client.getDB("Thangavel");
 		DBCollection collection = db.getCollection("CRUDSamples");
-		collection.drop();
+		//collection.drop();
+		insertDocument(collection);
+		readDocument(collection);
+		
+	}
+	
+	private static void insertDocument(DBCollection collection) throws UnknownHostException{
 		
 		DBObject dbObject = (DBObject) JSON.parse("{'Name':'Thangavel', 'Age':30, 'Address':'India'}");
 		DBObject OneDBObject = (DBObject) JSON.parse("{'Name':'Loganathan', 'Age':23, 'Address':'US'}");
@@ -31,5 +33,13 @@ public class SampleCRUDOps {
 		
 		collection.insert(dbObjects);
 		
+	}
+	
+	private static void readDocument(DBCollection collection){
+		DBCursor find = collection.find();
+		while (find.hasNext()) {
+			DBObject dbObject = (DBObject) find.next();
+			System.out.println(dbObject.toMap());
+		}
 	}
 }
