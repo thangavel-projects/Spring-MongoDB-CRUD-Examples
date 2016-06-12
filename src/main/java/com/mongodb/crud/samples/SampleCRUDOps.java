fd5196ff277ab4bdc0a949c95a7fae2281f5185b
Thangavel L Nathan <yel.thangavel@gmail.com>
2016-06-12 18:28:20 +0530

    MongoDB 3.2.2 upgraded and added few CRUD Ops.

--- src/main/java/com/mongodb/crud/samples/SampleCRUDOps.java
+++ src/main/java/com/mongodb/crud/samples/SampleCRUDOps.java
@@ -2,44 +2,134 @@ package com.mongodb.crud.samples;
 
 import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
-import com.mongodb.DBObject;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import com.mongodb.Block;
 import com.mongodb.MongoClient;
-import com.mongodb.util.JSON;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Projections;
+import com.mongodb.client.model.Sorts;
 
 public class SampleCRUDOps {
     
 	public static void main(String[] args) throws UnknownHostException {
+		@SuppressWarnings("resource")
 		MongoClient client = new MongoClient();
-		DB db = client.getDB("Thangavel");
-		DBCollection collection = db.getCollection("CRUDSamples");
-		//collection.drop();
-		insertDocument(collection);
-		readDocument(collection);
+		MongoDatabase db = client.getDatabase("Thangavel");
+		MongoCollection<Document> collection = db.getCollection("CRUDSamples");
+		collection.drop();
+		insertDocuments(collection);
+		readDocuments(collection);
+		readDocumentFeatures(collection);
 		
 	}
 	
-	private static void insertDocument(DBCollection collection) throws UnknownHostException{
+	private static void readDocumentFeatures(MongoCollection<Document> collection) {
+		// Sorting Document
+		System.out.println("###SORTED DOCUMENT###");
+		Block<Document> document = new Block<Document>() {
+			@Override
+			public void apply(Document doc) {
+				System.out.println(doc.toJson());
+			}
+		};
+		collection.find(Filters.exists("Age")).sort(Sorts.ascending("Address")).forEach(document);
 		
-		DBObject dbObject = (DBObject) JSON.parse("{'Name':'Thangavel', 'Age':30, 'Address':'India'}");
-		DBObject OneDBObject = (DBObject) JSON.parse("{'Name':'Loganathan', 'Age':23, 'Address':'US'}");
-		List<DBObject> dbObjects = new ArrayList<>();
-		dbObjects.add(dbObject);
-		dbObjects.add(OneDBObject);
+		// Use Projections
+		MongoCursor<Document> projections = collection.find().projection(Projections.include("Address","Age"))
+				.iterator();
 		
-		collection.insert(dbObjects);
-		
-	}
-	
-	private static void readDocument(DBCollection collection){
-		DBCursor find = collection.find();
-		while (find.hasNext()) {
-			DBObject dbObject = (DBObject) find.next();
-			System.out.println(dbObject.toMap());
+		System.out.println("###Use Projections#####");
+		while (projections.hasNext()) {
+			System.out.println(projections.next().toJson());
 		}
+		
+		// Use Filters
+		System.out.println("Use of Filters@@@@@");
+		Bson filter = Filters.and(Filters.lte("Age", "24"),Filters.eq("Address", "Muthur"));
+		//Bson projection = new Document("Name",1).append("_id", 0);
+		
+		Bson project = Projections.fields(Projections.include("Age","Name"),Projections.excludeId());
+		
+		
+		List<Document> filteredDocument = collection.find(filter)
+				.projection(project)
+				.into(new ArrayList<Document>());
+		for (Document newDoc : filteredDocument) {
+			System.out.println(newDoc.toJson());
+		}
+		
+	}
+
+	private static void insertDocuments(MongoCollection<Document> collection) throws UnknownHostException{
+		
+		Document singleDocument = new Document("Name", "Thangavel")
+				.append("Age", "23")
+				.append("Address", "India");
+		
+		// Insert Single Document
+		collection.insertOne(singleDocument);
+		
+		Document multiDocumentOne = new Document("Name", "Selvakumar")
+				.append("Age", "25")
+				.append("Address", "Thennilai");
+		
+		Document multiDocumentTwo = new Document("Name", "Karthik")
+				.append("Age", "24")
+				.append("Address", "Muthur");
+		
+		// Insert Multiple Document
+		collection.insertMany(Arrays.asList(multiDocumentOne,multiDocumentTwo));
+		
+	}
+	
+	private static void readDocuments(MongoCollection<Document> collection){
+		
+		Document first = collection.find().first();
+		// Print First Document Document
+		System.out.println(first.toJson());
+		
+		// Iterate All Document using Cursor
+		MongoCursor<Document> cursor = collection.find().iterator();
+		System.out.println("##Iterating Using Cursor##");
+		
+		try {
+			while (cursor.hasNext()) {
+				System.out.println(cursor.next().toJson());
+			} 
+		} finally {
+			// Require to close cursor always
+			cursor.close();
+		}
+		
+		// Read Document using Filters
+		System.out.println("##Iterating through Filters##");
+		Document find = collection.find(Filters.eq("Age", "24")).first();
+		System.out.println(find.toJson());
+		
+		System.out.println("##Iterating through Filters with Multiple Conditions##");
+		MongoCursor<Document> findDoc = collection.find(Filters.or(Filters.gt("Age", "24"),
+				Filters.eq("Address", "Muthur"))).iterator();
+		while(findDoc.hasNext()){
+			System.out.println(findDoc.next().toJson());
+		}
+		
+		// Use large set of Query
+		System.out.println("$$$$Large Set of Queries$$$$");
+		Block<Document> document = new Block<Document>() {
+			@Override
+			public void apply(Document doc) {
+				System.out.println(doc.toJson());
+			}
+		};
+		collection.find().forEach(document);
+		
 	}
 }
